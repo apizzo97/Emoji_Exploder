3d84d31025d01d2ae430cebe91cfb314ee77ce50
apizzo <adpizzo96@gmail.com>
2018-04-27 19:16:49 -0400

    I did some stuff

---
+++ assets.js
@@ -0,0 +1,16 @@
+
+//This file will contain functions for each of the assets
+
+
+var emojiAssets = {
+
+    game.load.spritesheet();
+    game.load.spritesheet();
+    game.load.spritesheet();
+    game.load.spritesheet();
+    game.load.spritesheet();
+    game.load.spritesheet();
+
+
+
+}
